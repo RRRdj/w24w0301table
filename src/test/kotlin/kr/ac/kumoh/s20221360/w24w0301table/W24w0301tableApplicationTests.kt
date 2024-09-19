031d92b15b3591eb748a03fd376c2b3a046da917
RRRdj <emforhs3213@naver.com>
2024-09-19 13:20:41 +0900

    HTML table practice

---
+++ src/test/kotlin/kr/ac/kumoh/s20221360/w24w0301table/W24w0301tableApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20221360.w24w0301table
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w0301tableApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
